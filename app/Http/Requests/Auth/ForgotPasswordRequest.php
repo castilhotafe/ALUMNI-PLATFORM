0142a6abb30f0261aa266c92d6475ae4fb67d95c
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2026-09-11 22:43:09 +0800

    feat(password_requests): create ForgotPassword and ResetPassword requests

---
+++ app/Http/Requests/Auth/ForgotPasswordRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ForgotPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email', 'exists:users,email'],
+        ];
+    }
+
+    /**
+     * Custom validation messages.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Please enter your account email address.',
+            'email.email' => 'Please provide a valid email address.',
+            'email.exists' => 'We couldn\'t find an account matching that email address.',
+        ];
+    }
+}
