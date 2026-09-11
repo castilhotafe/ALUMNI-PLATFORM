fa4955a8c569cc07fdeab441b7d099073b4ca5c9
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2026-09-11 22:58:09 +0800

    feat(password_reset_request): create password reset requests

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -58,8 +58,6 @@
 
     <!-- Back to Login -->
     <div class="auth-footer">
-        <p><a href="
-        <!-- {{ route('login') }} -->
-         " class="text-link-secondary"><i class="fas fa-arrow-left"></i> Back to login</a></p>
+        <p><a href="{{ route('login') }}" class="text-link-secondary"><i class="fas fa-arrow-left"></i> Back to login</a></p>
     </div> 
 @endsection
