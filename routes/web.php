a8d4bdc24cb29e9e6bdfb504566f0c182bf2ab9b
Marcos Castilho <20149083@tafe.wa.edu.au>
2026-09-15 11:24:33 +0800

    feat: add email verification notice and resend flow
    Refs #18

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +12,14 @@ Route::get('/', function () {
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
     ->middleware(['auth', 'signed'])
     ->name('verification.verify');
+
+Route::get('/verify-email', EmailVerificationPromptController::class)
+    ->middleware('auth')
+    ->name('verification.notice');
+
+Route::post('/email/verification-notification', EmailVerificationNotificationController::class)
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
 
 // Authenticated and Verified Routes (Only for logged-in users with verified email)
 Route::middleware(['auth', 'verified'])->group(function () {
