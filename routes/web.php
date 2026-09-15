ae7250eb4863065e39e79a8bdf6dbbb17f50c96e
Marcos Castilho <20149083@tafe.wa.edu.au>
2026-09-15 11:09:53 +0800

    feat: add authenticated signed email verification
    Refs #18

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed'])
+    ->name('verification.verify');
 
 // Authenticated and Verified Routes (Only for logged-in users with verified email)
 Route::middleware(['auth', 'verified'])->group(function () {
